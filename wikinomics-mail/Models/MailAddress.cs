4218586066e51d8a568273bb59f0cb1f4451ce71
Magnus Hjelmen Kristiansen <s333740@oslomet.no>
2021-01-06 17:20:04 +0100

    Classes for mail addresses and mails

---
+++ wikinomics-mail/Models/MailAddress.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace wikinomics_mail.Models
+{
+    //Object created when signing up for mailing list
+    public class MailAddress
+    {
+        //Auto increment ID in DB
+        public int Id { get; set; }
+
+        //Email address
+        public string Address { get; set; }
+
+        //A unique code used for unsubscribing from the mailing list
+        public string UniqueId { get; set; }
+    }
+}
