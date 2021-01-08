cc35044bafa8fdbccb8e443ab7dca58449ae6009
Jakob Olsrud Johansen <s333753@oslomet.no>
2021-01-08 13:08:41 +0100

    Push, nothing special

--- wikinomics-mail/ClientApp/src/app/home/home.component.ts
+++ wikinomics-mail/ClientApp/src/app/home/home.component.ts
@@ -3,6 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: [ './home.component.css' ]
 })
 export class HomeComponent {
+
+  showHeaderContent: Boolean = false;
+  showNumber: Number = 1;
+
+  constructor(){}
+
+  ShowHeaderContent() {
+    this.showHeaderContent = this.showHeaderContent ? false : true;
+  }
+
+  /*showRegistration() {
+    let info = document.getElementById("info");
+    let registration = document.getElementById("registration");
+    const btnShowReg = document.getElementById("btnShowReg");
+
+    if (info.className = "d-block") {
+      info.className = "d-none";
+      registration.className = "d-block";
+      btnShowReg.className = "d-none";
+    }
+
+  }*/
 }
