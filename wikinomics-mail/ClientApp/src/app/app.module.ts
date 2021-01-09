3cac9b741cf099f079ca5f3e5ace6528bcdc182b
Magnus Hjelmen Kristiansen <s333740@oslomet.no>
2021-01-09 15:38:55 +0100

    Login page and such, not working yet

--- wikinomics-mail/ClientApp/src/app/app.module.ts
+++ wikinomics-mail/ClientApp/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AdminComponent } from './admin/admin.component';
 import { MailModal } from './admin/modal/mailModal';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -25,6 +26,7 @@ import { MailModal } from './admin/modal/mailModal';
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'admin', component: AdminComponent, pathMatch: 'full' },
+      { path: 'login', component: LoginComponent, pathMatch: 'full' },
     ]),
     NgbModule
   ],
---
+++ wikinomics-mail/ClientApp/src/app/login/login.component.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, Inject } from '@angular/core';
+import { FormGroup, Validators, FormBuilder, FormControl } from '@angular/forms';
+import { DOCUMENT } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './login.component.html',
+})
+
+export class LoginComponent {
+  public loginForm: FormGroup;
+
+  constructor(private _http: HttpClient, private fb: FormBuilder, @Inject(DOCUMENT) private document: Document) {
+    this.loginForm = fb.group(this.formValidation);
+  }
+
+  formValidation = {
+    username: [
+      null, Validators.compose([Validators.required, Validators.minLength(2), Validators.maxLength(30)])
+    ],
+    password: [
+      null, Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(128)])
+    ]
+  }
+
+
+  ngOnInit() {
+    this.checkLogIn();
+  }
+
+  //Checks if you logged in, if not you are sent away from admin page
+  checkLogIn() {
+    this._http.get("api/Admin", { responseType: 'text' })
+      .subscribe(response => {
+        if (response == "Logged in") {
+          this.document.location.href = '/admin';
+        }
+      })
+  }
+
+}
