a9e768e47adcaa76fbc47314bdb8dbf07e72d24a
Jakob Olsrud Johansen <s333753@oslomet.no>
2021-01-12 11:22:39 +0100

    Slide in and slide out animation

--- wikinomics-mail/ClientApp/src/app/animations.ts
+++ wikinomics-mail/ClientApp/src/app/animations.ts
@@ -2,10 +2,13 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
 
 export let slide = trigger('slide', [
 
-    state('void', style({opacity: 0})),
+    
+    transition('void => *', [
+      style({transform: 'translateY(100px)', opacity: 0}),
+      animate('500ms 500ms')
+    ]),
 
-    transition(':enter', [
-      style({transform: 'translateY(100%)'}),
-      animate(500)
+    transition('* => void', [
+        animate('500ms', style({transform: 'translateY(-100%)', opacity: 0}))
     ])
 ]);
