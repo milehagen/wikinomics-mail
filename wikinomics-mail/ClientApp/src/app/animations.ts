70d91fb82bb35886bb176b077af567bee20e5e54
Jakob Olsrud Johansen <s333753@oslomet.no>
2021-01-12 12:35:33 +0100

    Minor changes

--- wikinomics-mail/ClientApp/src/app/animations.ts
+++ wikinomics-mail/ClientApp/src/app/animations.ts
@@ -3,12 +3,12 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
 export let slide = trigger('slide', [
 
     
-    transition('void => *', [
+    transition(':enter', [
       style({transform: 'translateY(100px)', opacity: 0}),
-      animate('500ms 500ms')
+      animate('400ms 600ms')
     ]),
 
-    transition('* => void', [
+    transition(':leave', [
         animate('500ms', style({transform: 'translateY(-100%)', opacity: 0}))
     ])
 ]);
