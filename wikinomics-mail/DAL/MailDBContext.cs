39f5cc1f35ac803dd466796d474897c3b70e29c7
Magnus Hjelmen Kristiansen <s333740@oslomet.no>
2021-01-15 20:23:46 +0100

    Testing

--- wikinomics-mail/DAL/MailDBContext.cs
+++ wikinomics-mail/DAL/MailDBContext.cs
@@ -19,25 +19,6 @@ namespace wikinomics_mail.DAL
         public byte[] Salt { get; set; }
     }
 
-    public class Statistics
-    {
-        [Key]
-        public int Id { get; set; }
-
-        //Last time some one signed up
-        public DateTime LastSignUp { get; set; }
-
-        //Total amount of people that have signed up
-        public int TotalSubscribes { get; set; }
-
-        //Current amount of people in the mailing list (TotalSubscribes - TotalUnsubscribes)
-        public int CurrentSubscribes { get; set; }
-
-        //Amount of people that have unsubscribed
-        public int TotalUnsubscribes { get; set; }
-    }
-
-
     public class MailDBContext : DbContext
     {
         public MailDBContext(DbContextOptions<MailDBContext> options) : base(options)
