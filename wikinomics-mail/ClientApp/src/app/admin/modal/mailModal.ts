ca28da87d917277b246d180436affa168b0ec4c2
Magnus Hjelmen Kristiansen <s333740@oslomet.no>
2021-01-15 18:30:46 +0100

    Fixed import

--- wikinomics-mail/ClientApp/src/app/admin/modal/mailModal.ts
+++ wikinomics-mail/ClientApp/src/app/admin/modal/mailModal.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClient } from '@angular/common/http';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { Mail } from '../Mail';
+import { Mail } from '../../Models/Mail';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 
