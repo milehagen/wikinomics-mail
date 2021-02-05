a11c2bfeaf7e084794c51df80d701777dcddf96f
Magnus Hjelmen Kristiansen <s333740@oslomet.no>
2021-02-05 15:06:33 +0100

    Changed azure account and deplyment, added first and last name to mail address objects

--- wikinomics-mail/Models/MailAddress.cs
+++ wikinomics-mail/Models/MailAddress.cs
@@ -11,6 +11,10 @@ namespace wikinomics_mail.Models
         //Auto increment ID in DB
         public int Id { get; set; }
 
+        public string Firstname { get; set; }
+
+        public string Lastname { get; set; }
+
         //Email address
         public string Address { get; set; }
 
